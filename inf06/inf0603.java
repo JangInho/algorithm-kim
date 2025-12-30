a55708906433b58c00f55e05430c9ae9c621b224
JangInho <wkddlsgh50@naver.com>
2025-12-30 15:48:19 +0900

    [0603] 삽입정렬

--- inf06/inf0603.java
+++ inf06/inf0603.java
@@ -1,8 +1,12 @@
-package inf06;
 
 import java.util.Scanner;
 
+
 public class inf0603 {
+
+    // 삽입 정렬
+    // j번째 arr값을 뒤로 계속 밀면서 i번째의 arr값의 제자리를 찾아주는 알고리즘임
+    // 모르겠으면 강의 그림을 보라
     
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -11,6 +15,23 @@ public class inf0603 {
         int[] arr = new int[n];
         for(int i =0 ; i< n; i++) {
             arr[i] = sc.nextInt();
+        }
+
+        for (int i =1; i< n; i ++) {
+            int tmp = arr[i];
+            int j;
+            for (j=i-1; j>= 0; j--) {
+                if (arr[j] > tmp) {
+                    arr[j+1] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j+1] = tmp;
+        }
+
+        for (int a : arr) {
+            System.out.print(a + " ");
         }
 
          
